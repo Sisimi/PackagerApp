b1d1337be4d61cc32185c607332ef0f357614770
Sisimi <tomi9994@gmail.com>
2020-03-23 09:34:24 +0100

    Error added
    Added an error to see if Travis works as intended.

--- src/app/src/main/java/com/example/packagerapp/FirstFragment.kt
+++ src/app/src/main/java/com/example/packagerapp/FirstFragment.kt
@@ -16,6 +16,7 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
+    Egy forditasi hiba
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
